f7f3303005680237066edbb47ad2b72fe0c42330
Rhomill <orinarhomill@gmail.com>
2023-09-28 21:36:32 -0500

    Added validations to house and student models

--- app/models/house.rb
+++ app/models/house.rb
@@ -1,3 +1,6 @@
 class House < ApplicationRecord
   has_many :students # One to many association
+
+  validates :name, :points, presence: true
+  validates :points, numericality: { only_integer: true }
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,5 @@
 class Student < ApplicationRecord
   belongs_to :house # One to many association
+
+  validates :name, presence: true # FK is also required.
 end
