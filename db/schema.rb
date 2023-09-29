7082b94be2c60bc7146fddbb296de54e626fbd65
Rhomill <orinarhomill@gmail.com>
2023-09-28 21:28:56 -0500

    Created house and student models

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_29_013225) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_29_014833) do
   create_table "houses", force: :cascade do |t|
     t.string "name"
     t.integer "points"
@@ -18,4 +18,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_29_013225) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "students", force: :cascade do |t|
+    t.string "name"
+    t.integer "house_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["house_id"], name: "index_students_on_house_id"
+  end
+
+  add_foreign_key "students", "houses"
 end
