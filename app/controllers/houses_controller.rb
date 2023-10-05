141db37f8f679db5844c068320663c9c22cce548
Rhomill <orinarhomill@gmail.com>
2023-10-05 15:43:31 -0500

    Added more code to the controllers and views

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -1,6 +1,11 @@
 class HousesController < ApplicationController
   def index
+    @houses = House.all # SELECT * FROM houses;
   end
+
+  # Rails automatically loads the associated view:
+  # app/views/houses/index.html.erb.
+  # All @instance vars are shared with view.
 
   def show
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
-  get 'houses/index'
-  get 'houses/show'
+  # GET / => houses#index
+
+  root to: 'houses#index'
+
+  # GET /houses/:id => houses#show
+
+  get "houses/:id", to: "houses#show", id: /\d+/
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
