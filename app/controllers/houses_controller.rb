ded55a7c97039c62596fd75d594fa03da850f576
Rhomill <orinarhomill@gmail.com>
2023-10-06 16:58:30 -0500

    Added a show page

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -5,8 +5,13 @@ class HousesController < ApplicationController
 
   # Rails automatically loads the associated view:
   # app/views/houses/index.html.erb.
-  # All @instance vars are shared with view.
+  # All @instance vars is shared with view.
 
+  # GET / houses/:id
   def show
+    @house = House.find(params[:id])
   end
+   # Rails automatically loads the associated view:
+  # app/views/houses/show.html.erb.
+  # All @house variable is shared with view.
 end
