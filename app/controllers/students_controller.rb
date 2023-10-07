f24ac365ddab91fc5f08b50acfb4e1598496f81a
Rhomill <orinarhomill@gmail.com>
2023-10-06 21:33:33 -0500

    Made teacher model

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,14 +1,16 @@
 class StudentsController < ApplicationController
   #GET /students
   def index
-    @students = Student.order(:name)
+  # The .includes removes our N+1 query problem
+  # Accessing the student houses in a loop in the view
+    @students = Student.includes(:house).order(:name)
   end
-  # Automatacclly renders the associated view:
+  # Automataclly renders the associated view:
   # app/views/students/index.html.erb
 
   # GET /students/:id => params[:id]
   def show
-    @student = Student.find(params[:id])
+    @student = Student.includes(:house).find(params[:id])
   end
   # Automatacclly renders the associated view:
   # app/views/students/show.html.erb
